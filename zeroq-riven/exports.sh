a56565f1091ffd31cb13f162bc902344fea14f13
Elvis <elvissalihovic.es@gmail.com>
2026-04-19 21:23:37 +0800

    Use Umbrel LAN IP for Riven auth origin

--- zeroq-riven/exports.sh
+++ zeroq-riven/exports.sh
@@ -1,3 +1,13 @@
 export APP_ZEROQ_RIVEN_URL="http://zeroq-riven_server_1:8080"
 export APP_ZEROQ_RIVEN_DATA_DIR="${EXPORTS_APP_DIR}/data/riven"
 export APP_ZEROQ_RIVEN_MOUNT_DIR="${UMBREL_ROOT}/data/storage/downloads/riven"
+export APP_ZEROQ_RIVEN_PORT="8080"
+
+local_ipv4s="$(hostname --all-ip-addresses 2>/dev/null | tr ' ' '\n' | grep -E '^[0-9]+\\.[0-9]+\\.[0-9]+\\.[0-9]+$' || true)"
+primary_local_ipv4="$(printf '%s\n' "${local_ipv4s}" | head -n 1)"
+
+if [[ -n "${primary_local_ipv4}" ]]; then
+  export APP_ZEROQ_RIVEN_LOCAL_URL="http://${primary_local_ipv4}:${APP_ZEROQ_RIVEN_PORT}"
+else
+  export APP_ZEROQ_RIVEN_LOCAL_URL="http://${DEVICE_DOMAIN_NAME}:${APP_ZEROQ_RIVEN_PORT}"
+fi
