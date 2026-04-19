958f924c87ce9808cc1e02693c0ce0de74097ff2
Elvis <elvissalihovic.es@gmail.com>
2026-04-19 19:10:42 +0800

    Convert Riven package into custom Umbrel store app

---
+++ zeroq-riven/exports.sh
@@ -0,0 +1,3 @@
+export APP_ZEROQ_RIVEN_URL="http://zeroq-riven_server_1:8080"
+export APP_ZEROQ_RIVEN_DATA_DIR="${EXPORTS_APP_DIR}/data/riven"
+export APP_ZEROQ_RIVEN_MOUNT_DIR="${UMBREL_ROOT}/data/storage/downloads/riven"
