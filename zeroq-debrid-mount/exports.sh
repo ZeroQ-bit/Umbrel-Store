07aceb8352e52a9d42325a5cfe8d94002a397a42
ZeroQ-bit <elvissalihovic.es@gmail.com>
2026-06-01 16:17:09 +0800

    Add standalone Debrid Mount app

---
+++ zeroq-debrid-mount/exports.sh
@@ -0,0 +1,4 @@
+export APP_ZEROQ_DEBRID_MOUNT_DIR="${UMBREL_ROOT}/home/Downloads/.vortexo-source"
+export APP_ZEROQ_DEBRID_MOUNT_CONFIG_DIR="${EXPORTS_APP_DIR}/data/config"
+export APP_ZEROQ_DEBRID_MOUNT_STATUS_DIR="${EXPORTS_APP_DIR}/data/status"
+export APP_ZEROQ_DEBRID_MOUNT_VORTEXO_PATH="/downloads/.vortexo-source"
